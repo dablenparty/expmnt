8b02ef61c6ade328c65e30f809d5054fce2b69c6
dablenparty <hdavenport8902@gmail.com>
2023-11-02 13:34:50 -0400

    Init Rust submodules

---
+++ rust/src/lib.rs
@@ -0,0 +1,9 @@
+/*!
+Welcome to EXPMNT Rust. This library is a collection
+of all the programming experiments I've done in Rust.
+They're all independent of each other, but I've set up
+this library via Cargo and git submodules to make it
+easier to manage & view. If you haven't already, clone
+everything and run `cargo doc --open` to view the docs,
+then select a crate from the sidebar.
+*/
